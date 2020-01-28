ae992f44a44f81d1501cb5f7f4b77b03102a2e82
Alex Cope <alexcope@google.com>
2020-01-28 16:10:21 -0500

    fix style

--- src/jwks.cc
+++ src/jwks.cc
@@ -297,7 +297,7 @@ Status extractX509(const std::string& key, Jwks::Pubkey* jwk) {
   }
   bssl::UniquePtr<EVP_PKEY> tmp_pkey(X509_get_pubkey(jwk->x509_.get()));
   if (tmp_pkey == nullptr) {
-    return Status::JwksX509GetPubkeyError; 
+    return Status::JwksX509GetPubkeyError;
   }
   jwk->rsa_.reset(EVP_PKEY_get1_RSA(tmp_pkey.get()));
   if (jwk->rsa_ == nullptr) {
